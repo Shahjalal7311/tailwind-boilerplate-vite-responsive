9ca6cf87fd2a570cb4346158056210788a94bb7c
shahjalal <shah.jalal@reivo.jp.com>
2021-09-21 12:46:39 +0600

    post css and tailwind configure with vite

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens: {
+        'tablet': '768px',
+        // => @media (min-width: 640px) { ... }
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
